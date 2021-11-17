bd91a55298eb9f38e5f46628deac36d596259f7e
karendaa <karendaa@stud.ntnu.no>
2021-11-17 23:28:11 +0100

    changed structure and added bootstrap for easier implementation of responsive design

---
+++ script/script.js
@@ -0,0 +1,20 @@
+//Load all different parts of page
+$(function () {
+  $("#navigation").load("../components/navigation.html");
+});
+
+$(function () {
+  $("#footer").load("../components/footer.html");
+});
+
+$(function () {
+  $("#search").load("../components/search.html");
+});
+
+$(function () {
+  $("#top_posts").load("../components/top_posts.html");
+});
+
+$(function () {
+  $("#top_users").load("../components/top_users.html");
+});
