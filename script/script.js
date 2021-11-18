5cdf7c686db79908819e719bb5f6b1a0874ec0b1
karendaa <karendaa@stud.ntnu.no>
2021-11-18 09:35:13 +0100

    move all load functions into one

--- script/script.js
+++ script/script.js
@@ -1,31 +1,11 @@
 //Load all different parts of page
 $(function () {
   $("#navigation").load("../components/navigation.html");
-});
-
-$(function () {
   $("#main_image").load("../components/main_image.html");
-});
-$(function () {
   $("#login").load("../components/login.html");
-});
-
-$(function () {
   $("#signup").load("../components/signup.html");
-});
-
-$(function () {
   $("#footer").load("../components/footer.html");
-});
-
-$(function () {
   $("#search").load("../components/search.html");
-});
-
-$(function () {
   $("#top_posts").load("../components/top_posts.html");
-});
-
-$(function () {
   $("#top_users").load("../components/top_users.html");
 });
