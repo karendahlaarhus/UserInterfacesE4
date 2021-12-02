c93d48d111114ed0968c18235bd2e7d2c4c98e0e
karendaa <karendaa@stud.ntnu.no>
2021-12-02 10:42:30 +0100

    add popups for top posts

--- script/popup.js
+++ script/popup.js
@@ -2,3 +2,8 @@ var togglePopup = (popup) => {
   var popupModal = document.getElementById(popup);
   popupModal.classList.toggle("show_popup");
 };
+
+var toggleMapPopup = (popup) => {
+  var popupMapModal = document.getElementById(popup);
+  popupMapModal.classList.toggle("show_map_popup");
+};
--- script/script.js
+++ script/script.js
@@ -6,6 +6,7 @@ $(function () {
   $("#footer").load("../components/footer.html");
   $("#search").load("../components/search.html");
   $("#top_posts").load("../components/top_posts.html");
+  $("#top_posts_popup").load("../components/top_posts_popup.html");
   $("#top_users").load("../components/top_users.html");
   $("#user").load("../components/userprofile.html");
   $("#home_title").load("../components/title_home.html");
