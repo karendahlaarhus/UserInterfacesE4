2aed4fcb76bb6c2c0f340cdab699455ef8541b5f
karendaa <karendaa@stud.ntnu.no>
2021-12-09 13:15:01 +0100

    add search on username

--- script/search.js
+++ script/search.js
@@ -1,11 +1,10 @@
 // Function to search for experiences
-function myFunction() {
-  var input, filter, cards, cardContainer, title, i, authors;
+function search() {
+  var input, filter, cards, cardContainer, title, i;
   input = document.getElementById("search_input");
   filter = input.value.toUpperCase();
   cardContainer = document.getElementById("top_experiences");
   cards = cardContainer.getElementsByClassName("card");
-  authors = cardContainer.getElementsByClassName("card");
 
   // Search in card text for the value from input
   for (i = 0; i < cards.length; i++) {
@@ -14,6 +13,22 @@ function myFunction() {
       cards[i].style.display = "";
     } else {
       cards[i].style.display = "none";
+    }
+  }
+
+  var input1, filter1, cards1, cardContainer1, title1, k;
+  input1 = document.getElementById("search_input");
+  filter1 = input1.value.toUpperCase();
+  cardContainer1 = document.getElementById("top_users");
+  cards1 = cardContainer1.getElementsByClassName("card");
+
+  // Search in card text for the value from input
+  for (k = 0; k < cards1.length; k++) {
+    title1 = cards1[k].querySelector(".card-text");
+    if (title1.innerText.toUpperCase().indexOf(filter1) > -1) {
+      cards1[k].style.display = "";
+    } else {
+      cards1[k].style.display = "none";
     }
   }
 }
